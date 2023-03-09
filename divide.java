5e2f8c5cc62a462516fd8a89cead82adcd68688b
Arihant Jain <mail.jainarihant@gmail.com>
2023-03-09 18:28:23 +0530

    ADDING DIVIDE

---
+++ divide.java
@@ -0,0 +1,9 @@
+public class divide {
+    public static void main(String[] args) {
+        int c = 7;
+        int d = 3;
+        int divide = c/d;
+        System.out.println("YOUR DIVIDE IS : " + divide);
+    }
+}
+
