7a9c1f4626e0e8cb0774781d64c85ab525eda6b5
Arihant Jain <mail.jainarihant@gmail.com>
2023-03-09 18:07:06 +0530

    add multiply

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multiply {
+    public static void main(String[] args) {
+        int c = 7;
+        int d = 3;
+        int multiply = c*d;
+        System.out.println("YOUR MULTIPLY IS : " + multiply);
+    }
+}
